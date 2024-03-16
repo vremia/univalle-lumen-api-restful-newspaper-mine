d4b0ecdb524ea93546a196dda5ac47f3dbffbe2b
vremia <maday.alcala.cuba@gmail.com>
2024-03-15 20:31:55 -0400

    Model created for Content.

---
+++ app/Models/Content.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'pretitle',
+        'title',
+        'alias',
+        'author',
+        'image_url',
+        'introduction',
+        'body',
+        'format',
+        'status',
+        'edition_date',
+        'category_title',
+        'category_alias',
+        'created_at',
+        'updated_at',
+        'created_by',
+        'updated_by',
+        'category_id'
+    ];
+}
